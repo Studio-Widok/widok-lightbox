dfdce14dd5162cd99574254a80220ae088458994
weterynarzfred <weterynarzfred@gmail.com>
2020-11-10 14:20:54 +0100

    first commit

---
+++ widok-lightbox.js
@@ -0,0 +1,107 @@
+const $ = require('cash-dom');
+const widok = require('widok');
+
+class Lightbox {
+  constructor(options) {
+    this.prepareOption(options);
+    this.sources = [];
+    $(this.options.source).each((id, element) =>
+      this.sources.push(new Source(element, id, this))
+    );
+    this.prepareLightbox();
+
+    this.isShown = false;
+  }
+
+  prepareOption(options) {
+    this.options = {
+      wrap: undefined,
+      source: '.lightbox-source',
+      close: undefined,
+    };
+    Object.assign(this.options, options);
+  }
+
+  prepareLightbox() {
+    if (this.options.wrap === undefined) {
+      this.wrap = $('<div class="lightbox-wrap"></div>').appendTo('body');
+    } else this.wrap = $(this.options.wrap);
+    this.wrap.on('click', () => this.hide());
+
+    this.sizer = $('<div class="lightbox-sizer"></div>').appendTo(this.wrap);
+    this.image = $('<img class="lightbox-image">')
+      .appendTo(this.sizer)
+      .on('click', event => {
+        event.stopPropagation();
+      });
+
+    if (this.options.close !== undefined) {
+      this.close = $(this.options.close).on('click', this.hide);
+    }
+  }
+
+  show(source) {
+    if (!this.isShown) {
+      this.wrap.addClass('shown');
+      this.isShown = true;
+    }
+    this.image.attr({
+      src: source.url,
+    });
+    this.currentImage = source.id;
+    this.resize();
+  }
+
+  hide() {
+    if (this.isShown) {
+      this.wrap.removeClass('shown');
+      this.isShown = false;
+    }
+  }
+
+  resize() {
+    if (this.isShown) {
+      const ratio = this.sources[this.currentImage].ratio;
+      if (ratio > widok.w / widok.h) {
+        this.image.css({
+          width: '100%',
+          height: (widok.w / widok.h / ratio) * 100 + '%',
+        });
+      } else {
+        this.image.css({
+          width: (ratio / widok.w) * widok.h * 100 + '%',
+          height: '100%',
+        });
+      }
+    }
+  }
+}
+
+class Source {
+  constructor(element, id, lightbox) {
+    this.element = $(element);
+    this.id = id;
+    this.lightbox = lightbox;
+
+    this.url = this.element.data('full-src');
+    this.ratio = this.element.data('ratio');
+
+    this.element.on('click', event => {
+      this.lightbox.show(this);
+    });
+  }
+}
+
+const lightboxes = [];
+
+window.addEventListener('afterLayoutChange', () => {
+  lightboxes.forEach(lightbox => lightbox.resize());
+});
+
+function createLightbox(options) {
+  const lightbox = new Lightbox(options);
+  lightboxes.push(lightbox);
+  return lightbox;
+}
+
+module.exports = createLightbox;
